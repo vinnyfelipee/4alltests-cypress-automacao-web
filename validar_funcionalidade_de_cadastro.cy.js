8669e8feca0333b4fadd2c546b83619e7b6b25a5
Vinny Felipe <117942543+vinnyfelipee@users.noreply.github.com>
2024-02-16 19:54:08 -0300

    Add files via upload

---
+++ validar_funcionalidade_de_cadastro.cy.js
@@ -0,0 +1,32 @@
+describe('Funcionalidade Cadastro', () => {
+
+  it('Validar cadastro validado com suceesso', () => {
+    cy.visit('http://aprendendotestar.com.br/treinar-automacao.php')
+    cy.get(':nth-child(2) > td > input').type('user')
+    cy.get(':nth-child(4) > td > input').type('12345')
+    cy.get(':nth-child(6) > td > input').type('vinny')
+    cy.get('td > .btn').click()
+    cy.get('tbody > :nth-child(2) > :nth-child(2)').should('have.text', 'vinny')
+  })
+
+  it('Validar cadastro sem informar usuario', () => {
+    cy.visit('http://aprendendotestar.com.br/treinar-automacao.php')
+    cy.get(':nth-child(4) > td > input').type('12345')
+    cy.get(':nth-child(6) > td > input').type('Vinny')
+    cy.get('td > .btn').click() 
+    cy.get('input[name="form_usuario"]').should('have.attr', 'required')
+
+  })
+  
+  it('Validar apagar usuario', () => {
+     cy.visit('http://aprendendotestar.com.br/treinar-automacao.php')
+    cy.get('tbody > :nth-child(2) > :nth-child(2)').should('have.text', 'vinny')
+    cy.get(':nth-child(2) > :nth-child(5) > a').click()
+    cy.visit('http://aprendendotestar.com.br/treinar-automacao.php')
+    cy.get('tbody > :nth-child(2) > :nth-child(2)').should('not.have.text', 'vinny')
+
+  })
+
+
+
+})
